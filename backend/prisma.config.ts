3a4e56da3c1b5f46ccede990797af9083278bfb2
bustEXZ <bustexz@yandex.ru>
2026-05-13 20:16:16 +0000

    feat: добавить автомиграцию

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -3,9 +3,11 @@ import { defineConfig } from '@prisma/config';
 // Если процесс запущен внутри Docker, используем 'db', иначе 'localhost'
 const dbHost = process.env.IS_DOCKER === 'true' ? 'db' : 'localhost';
 
+console.log("DB Connect: ", process.env.DATABASE_URL);
+
 export default defineConfig({
   datasource: {
     // ВНИМАНИЕ: проверьте, что user/password/wedding_db совпадают с docker-compose.yml
-    url: `postgresql://user:password@${dbHost}:5432/wedding_db`,
+    url: process.env.DATABASE_URL || `postgresql://user:password@${dbHost}:5432/wedding_db`,
   },
 });
